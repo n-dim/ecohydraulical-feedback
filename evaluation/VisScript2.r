c67840e4b75158501840f425dc642a27f7a8fa16
n-dim <n@ndim.de>
2013-04-04 19:15:05 +0200

    optimized CSVread() to faster read raster sets

--- evaluation/VisScript2.r
+++ evaluation/VisScript2.r
@@ -2,9 +2,12 @@ library(fields)
 #library(rgl)
 
 readCSV <- function(file=NA) {
+  # Choose File with input parameters to read in first
   if(is.na(file)) file <- file.choose()
   Temp <- read.table(file, sep="=", colClasses="character", strip.white=T, nrows=35)
+  # reform as list
   parameter <- as.list(Temp[,2])
+  # give list elements the right names
   names(parameter) <- Temp[,1]
   formats <- c(rep("character", 2), "logical", rep("numeric", 25), rep("logical", 7))
   for(i in 1:length(parameter)){
@@ -12,17 +15,24 @@ readCSV <- function(file=NA) {
   }
 
   attach(parameter)
+  
+  # prepare raster read in
   rasterSets <- c("bareE", "discharge", "eTActual", "flowdirections", "flowResistance", "store", "topography", "vegetation")
   rasters <- vector("list", length(rasterSets))
+  # read in rasters
   for(i in 1:length(rasterSets)){
+        file <- file(paste("../model/output/", title, "_", rasterSets[i], ".csv", sep=""), open="rt")
     for(j in 1:nSteps){
-        Temp <- as.matrix(read.table(file=paste("../model/output/", title, "_", rasterSets[i], ".csv", sep=""), sep=";", skip=(1+(j-1)*(m+1)), nrow=m, header=F))
+        Temp <- as.matrix(read.table(file=file, sep=";", skip=1, nrow=m, header=F))
         colnames(Temp) <- NULL  
         rasters[[i]][[j]] <- Temp[1:n,1:m]
     }
+    close(file)
   }
+  # rename it
   names(rasters) <- rasterSets
   
+  # read summary file
   Summary <- read.table(file=paste("../model/output/", title, "_", "SummaryResults.csv", sep=""), sep=";", header=T)[,-8] #last column (#8) is empty
   
   detach("parameter")

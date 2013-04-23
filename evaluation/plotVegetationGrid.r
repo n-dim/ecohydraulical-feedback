45ee40b162f55de57b9bb45f8e6d120bb7863294
n-dim <n@ndim.de>
2013-04-23 15:01:05 +0200

    plot vegetation grid

---
+++ evaluation/plotVegetationGrid.r
@@ -0,0 +1,23 @@
+plotVegetationGrid <- function(data, time=1:data$parameter$nSteps){
+  library(fields)
+  attach(data$parameter)
+  
+  colsVeg <- c("white", two.colors(n=10, start="yellow", end="green4", middle="green"))
+  
+  for (i in time){
+    image.plot(1:n, 1:m, data$rasters$vegetation[[i]], asp=n/m, col=colsVeg, nlevel=10, 
+               main=paste0('simulation run "', title, '", timestep ', i), zlim=c(0,10))
+    
+  }
+  detach(data$parameter)
+  
+}
+
+#to plot all:
+#plotVegetationGrid(Test) 
+
+#to plot only one:
+#plotVegetationGrid(Test, 1)
+
+#to plot multiple:
+#plotVegetationGrid(Test, c(1,50,100,150,200))

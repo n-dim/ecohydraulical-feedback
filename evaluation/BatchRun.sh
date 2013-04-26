cef3e1ab51c9d9816335b174dfc6146cddb46580
n-dim <n@ndim.de>
2013-04-26 12:11:32 +0200

    computation time in  BatchRun

--- evaluation/BatchRun.sh
+++ evaluation/BatchRun.sh
@@ -5,9 +5,14 @@ if [ "$1" == "" ]; then
     echo "usage" 
     echo "./BatchRun.sh <inputParameterFile> <outputFolder>"
 else
+  start_time=$(date +%s)
 
   gfortran ../model/ecohydModel.f90 -o ../model/ecohydModel.out &&    # compilation
   ../model/ecohydModel.out "$1" "$2" &&                               # simulation run
   ./readAllCSV.sh "$2"                                                # convert to .RData
+
+  finish_time=$(date +%s)
+  echo "Calculation time: $((finish_time - start_time)) secs = $(((finish_time - start_time) / 60)) minutes"
+
 
 fi

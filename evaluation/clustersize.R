5b49012fffe875a98f5c56ad7227d53b4564fd4a
n-dim <n@ndim.de>
2013-05-03 22:04:44 +0200

    calculate cluster sizes with parallel computing

--- evaluation/clustersize.R
+++ evaluation/clustersize.R
@@ -28,8 +28,8 @@ getClusters <- function(grid){
       }
       
     }
-    
   }
+
   
   for(j in 2:m){
     thisrow <- which(clusters$row==j)
@@ -51,67 +51,21 @@ getClusterSizes <- function(clusters){
 } 
 
 clusterSizeProgression <- function(data){
+  library(doMC) # multicore calculations
+  registerDoMC(detectCores()) 
+  
   with(c(data$rasters, data$parameter), {
    
-   clusters <- list(NULL)
+   clusters <- foreach(i=1:nSteps) %dopar% getClusters(vegetation[[i]][2:m,2:n]>0)
+
    clusterSizes <- list(NULL)
    clusterSizesStat <- as.data.frame(t(rep(0,6)))
    names(clusterSizesStat) <- names(summary(0))
-   for (i in 1:nSteps){
-     clusters[[i]] <- getClusters(vegetation[[i]])
+   
+   for( i in 1:nSteps){
      clusterSizes[[i]] <- getClusterSizes(clusters[[i]]) 
-     clusterSizesStat[i,] <- summary(clusterSizes[[i]]$size)
+     clusterSizesStat[i,] <- summary(clusterSizes[[i]]$size) 
    }
-   plot(clusterSizesStat$Mean, type="l", ylab="cluster size [cells]", xlab="time [y]", main=paste("simulation run", title))
+   plot(clusterSizesStat$Mean, type="l", ylab="mean cluster size [cells]", xlab="time [y]", main=paste("simulation run", title))
   })
-}
-
-clusterSizeProgression(data)
-
-grid <- Test$rasters$vegetation[[200]]>0
-
-image(grid)
-
-clusters <- getClusters(grid)
-
-clusterSizes <- getClusterSizes(clusters) 
-  
-
-summary(clusterSizes$size)
-
-with(Test$parameter,
-
-  boxplot(clusterSizesStat, main=paste("simulation run", title))
-
-)
-plotVegetationGrid(Test, time=200)
-
-
-##############################################
-
-library(fields)
-colsVeg <- two.colors(n=9, start="yellow", end="green4", middle="green")
-
-for(i in 1:4){
-    
-  grid <- Run03$rasters$vegetation[[i]]
-  # n <- ncol(grid)
-  # m <- nrow(grid)
-  # image(1:n, 1:m, grid,zlim=c(1,9),col=colsVeg)
-  
-  threshold <- 0
-  th_grid <- matrix(as.numeric(grid<=threshold), ncol=ncol(grid))
-  
-  system.time(clusters <- getClusters(grid, threshold))
-  
-  #pdf(file=Run03$parameter$title, paper="a4")
-  image(1:m,1:n,th_grid, col=c("green","white"), main=paste("simulation run ", Run03$parameter$title, "timestep ", i))
-  text(labels=clusters$cluster, x=clusters$row, y=clusters$startcol, cex=0.5)
-  #dev.off()
-  
-  agClusters <- aggregate(size ~ cluster, data=clusters, FUN="sum")
-  cat('number of clusters: ', nrow(agClusters))
-  cat('Cluster sizes: ')
-  print(summary(agClusters$size))
-
 }

4cd5f030c9a0c4915a37aa7e2df4f8461399d1ba
n-dim <n@ndim.de>
2014-01-07 11:42:19 +0100

    threads; read input file

--- evaluation/InputCombination.R
+++ evaluation/InputCombination.R
@@ -6,23 +6,25 @@ data <- list(n=10, m=10, p=4, l=c(3,4,5), t=rep(c(6,7), each=3))
 
 data <- list(a=c(1,4,5), b=c(2,3,20), c=c(10,11,12))
 
-loop <- function(data, simName, Envir, Counter){
+loop <- function(data, Envir, Counter){
+  if(Counter$threadSplit==1) Counter$threadSplit = 10000000
+  
   for(i in 1:length(data[[1]])){
       assign(names(data[1]), data[[1]][i], envir=Envir)
       #print(paste(names(data[1]),"=", data[[1]][i]))
       if(!length(data)<=1){
-        loop(tail(data, -1), simName, Envir, Counter)
+        loop(tail(data, -1), Envir, Counter)
       }else{
           objects <- ls(pos=Envir)
           
           if(Counter$simRunNo %% Counter$threadSplit == 1){
            
               Counter$simFile <- paste0(Counter$file, "_No", Counter$simRunNo, "ff.txt")
-              write("", file=Counter$simFile)
+              write("!eco-hydraulical simulation input file", file=Counter$simFile)
 
           }
           
-          write(paste("\n!----", simName, "- No.", get("simRunNo", Counter), "\n"), file=Counter$simFile, append=T)
+          write(paste("\ntitle =", Counter$simName, " No.", get("simRunNo", Counter), "\n!---------------------\n"), file=Counter$simFile, append=T)
           
           
           for(obj in objects){
@@ -44,8 +46,9 @@ write.EcoHyd.Input <- function(data, folder= "/media/Data/eco-hyd", simName= "Te
     Envir <- new.env()
     Counter <- new.env()
     Counter$simRunNo <- 1
+    Counter$simName <- simName
   
-    simFolder <- file.path(folder,paste0("simRun_", format(Sys.time(), "%Y-%m-%d_%H-%M-%S_"), simName))
+    simFolder <- file.path(folder, paste0("simRun_", format(Sys.time(), "%Y-%m-%d_%H-%M-%S_"), simName))
     dir.create(simFolder)
     Counter$file <- file.path(simFolder,paste0(simName))
     
@@ -53,9 +56,9 @@ write.EcoHyd.Input <- function(data, folder= "/media/Data/eco-hyd", simName= "Te
     for(i in 1:length(data)){
       combinations <- combinations * length(data[[i]])
     }
-    Counter$threadSplit <- round(combinations/threads)
+    Counter$threadSplit <- ceiling(combinations/threads)
        
-    loop(data, simName, Envir, Counter)
+    loop(data, Envir, Counter)
     
     rm(Envir)
     rm(Counter)
@@ -63,7 +66,28 @@ write.EcoHyd.Input <- function(data, folder= "/media/Data/eco-hyd", simName= "Te
   
 }
 
-write.EcoHyd.Input(data, threads=3)
+
+#---- read parameters from file ----
+pars <- read.table("/media/Data/eco-hyd/exampleParameters.txt", sep="=", comment.char="!", fill=T, allowEscapes=T, encoding="UTF-8", strip.white=T, as.is=T)
+
+parlist <- t(pars[,2])
+colnames(parlist) <- pars[,1]
+parlist <- as.list(parlist[1,])
+as.numeric(parlist,)
+nums <- which(!is.na(as.numeric(parlist)))
+parlist[nums] <- as.numeric(parlist[nums])
+
+parlist$title <- NULL
+
+#---- change parameters ----
+
+parlist$roughness <- 10^c(-1:1)
+
+
+#---- write parameter cascade ----
+
+write.EcoHyd.Input(parlist, threads=3)
+
 
 
 

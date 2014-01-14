5289568ba360b780aad62cefe3a88843321355bd
n-dim <n@ndim.de>
2014-01-14 15:31:39 +0100

    folders in CSVtoRData

--- evaluation/CSVtoRData.r
+++ evaluation/CSVtoRData.r
@@ -2,10 +2,11 @@ source("readCSV.r")
 
 CSVtoRData <- function(file) {  #choose ..._inputParameter.txt as input File
   if(is.na(file)) file <- file.choose()
+  folder <- dirname(file)
   Data <- readCSV(file)
   outFileName <- Data$parameter$title
   assign(outFileName, Data)
-  save(list=outFileName, file=paste(outFileName, "_grids.RData", sep=""))
+  save(list=outFileName, file=file.path(folder,paste(outFileName, "_grids.RData", sep="")))
   #save(list=c(parameter=Data$parameter), file=paste(outFileName), sep="", "_inputParameter.RData")
   
   #dump(ls(Data), file=paste(outFileName, ".RData", sep=""))
